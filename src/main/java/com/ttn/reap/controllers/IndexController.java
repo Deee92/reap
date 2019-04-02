dc4478c19bf7fa64213bbbe8d9eeb8c04c748394
Deepika Tiwari <deepikatiwari92@gmail.com>
2019-04-02 17:16:54 +0530

    Handle 404, 500 errors

---
+++ src/main/java/com/ttn/reap/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.ttn.reap.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    ModelAndView index() {
+        return new ModelAndView("index");
+    }
+}
